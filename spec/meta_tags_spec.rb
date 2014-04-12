ae98bb60ddd5bb749d571ca9acf7834dcc5a2c3f
Sven Schwyn <schwyn@bitcetera.com>
2014-04-12 14:42:11 +0200

    Implement mirrored values inside namespaces declared as hashes.
    Inspired by the outdated pull request #15 of @lehni

--- spec/meta_tags_spec.rb
+++ spec/meta_tags_spec.rb
@@ -216,15 +216,15 @@ describe MetaTags::ViewHelper do
       subject.display_meta_tags.should eq('')
     end
 
-    it 'should display keywords when "keywords" used' do
-      subject.keywords('some-keywords')
-      subject.display_meta_tags(:site => 'someSite').should include('<meta content="some-keywords" name="keywords" />')
-    end
+     it 'should display keywords when "keywords" used' do
+       subject.keywords('some-keywords')
+       subject.display_meta_tags(:site => 'someSite').should include('<meta content="some-keywords" name="keywords" />')
+     end
 
-    it 'should display keywords when "set_meta_tags" used' do
-      subject.set_meta_tags(:keywords => 'some-keywords')
-      subject.display_meta_tags(:site => 'someSite').should include('<meta content="some-keywords" name="keywords" />')
-    end
+     it 'should display keywords when "set_meta_tags" used' do
+       subject.set_meta_tags(:keywords => 'some-keywords')
+       subject.display_meta_tags(:site => 'someSite').should include('<meta content="some-keywords" name="keywords" />')
+     end
 
     it 'should display default keywords' do
       subject.display_meta_tags(:site => 'someSite', :keywords => 'some-keywords').should include('<meta content="some-keywords" name="keywords" />')
@@ -456,6 +456,13 @@ describe MetaTags::ViewHelper do
       subject.display_meta_tags(:site => 'someSite').tap do |content|
         content.should_not include('<meta content="" property="og:title" />')
         content.should_not include('<meta content="" property="og:description" />')
+      end
+    end
+
+    it "should display mirrored content" do
+      subject.set_meta_tags(:title => 'someTitle')
+      subject.display_meta_tags(:open_graph => { :title => :title }).tap do |content|
+        content.should include('<meta content="someTitle" property="og:title" />')
       end
     end
   end
