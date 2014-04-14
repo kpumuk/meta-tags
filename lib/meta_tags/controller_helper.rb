a5b8a891539c4e1634e8302dc821139dcccbc455
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2014-04-14 16:10:49 -0400

    Use ActiveSupport::Concern for ControllerHelper

--- lib/meta_tags/controller_helper.rb
+++ lib/meta_tags/controller_helper.rb
@@ -6,39 +6,39 @@ module MetaTags
   #   @page_description = 'Member login page.'
   #   @page_keywords = 'Site, Login, Members'
   #
-  # Also you can use {InstanceMethods#set_meta_tags} method, that have the same parameters
+  # Also you can use {#set_meta_tags} method, that have the same parameters
   # as {ViewHelper#set_meta_tags}.
   #
   module ControllerHelper
-    def self.included(base)
-      base.send :include, InstanceMethods
-      base.alias_method_chain :render, :meta_tags
+    extend ActiveSupport::Concern
+
+    included do
+      alias_method_chain :render, :meta_tags
     end
 
-    module InstanceMethods
-      # Processes the <tt>@page_title</tt>, <tt>@page_keywords</tt>, and
-      # <tt>@page_description</tt> instance variables and calls +render+.
-      def render_with_meta_tags(*args, &block)
-        self.meta_tags[:title]       = @page_title       if @page_title
-        self.meta_tags[:keywords]    = @page_keywords    if @page_keywords
-        self.meta_tags[:description] = @page_description if @page_description
+    # Processes the <tt>@page_title</tt>, <tt>@page_keywords</tt>, and
+    # <tt>@page_description</tt> instance variables and calls +render+.
+    def render_with_meta_tags(*args, &block)
+      self.meta_tags[:title]       = @page_title       if @page_title
+      self.meta_tags[:keywords]    = @page_keywords    if @page_keywords
+      self.meta_tags[:description] = @page_description if @page_description
 
-        render_without_meta_tags(*args, &block)
-      end
-
-      # Set meta tags for the page.
-      #
-      # See <tt>MetaTags::ViewHelper#set_meta_tags</tt> for details.
-      def set_meta_tags(meta_tags)
-        self.meta_tags.update(meta_tags)
-      end
-      protected :set_meta_tags
-
-      # Get meta tags for the page.
-      def meta_tags
-        @meta_tags ||= MetaTagsCollection.new
-      end
-      protected :meta_tags
+      render_without_meta_tags(*args, &block)
     end
+    protected :render_with_meta_tags
+
+    # Set meta tags for the page.
+    #
+    # See <tt>MetaTags::ViewHelper#set_meta_tags</tt> for details.
+    def set_meta_tags(meta_tags)
+      self.meta_tags.update(meta_tags)
+    end
+    protected :set_meta_tags
+
+    # Get meta tags for the page.
+    def meta_tags
+      @meta_tags ||= MetaTagsCollection.new
+    end
+    protected :meta_tags
   end
 end
