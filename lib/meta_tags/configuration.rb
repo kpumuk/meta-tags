a6db9c490ce4bed691bfa7436711df68e6dfe3ed
Elsopuro <elsopuro@gmail.com>
2016-10-25 14:05:12 +0300

    Truncate site_title instead of title

--- lib/meta_tags/configuration.rb
+++ lib/meta_tags/configuration.rb
@@ -10,12 +10,16 @@ module MetaTags
     # Keywords separator - a string to join keywords with.
     attr_accessor :keywords_separator
 
+    # Truncate site_title instead of title, default: false
+    attr_accessor :truncate_site_title
+
     # Initializes a new instance of Configuration class.
     def initialize
       @title_limit = 70
       @description_limit = 160
       @keywords_limit = 255
       @keywords_separator = ', '
+      @truncate_site_title = false
     end
   end
 end
