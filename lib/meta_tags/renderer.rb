024e3dfd7cc00d1b3429526d556184dcd576930f
Steven Cook <stevenjamescook@gmail.com>
2016-12-06 22:33:52 +0000

    added :amphtml as an option to the renderer

--- lib/meta_tags/renderer.rb
+++ lib/meta_tags/renderer.rb
@@ -151,7 +151,7 @@ module MetaTags
     # @param [Array<Tag>] tags a buffer object to store tag in.
     #
     def render_links(tags)
-      [ :canonical, :prev, :next, :author, :publisher, :image_src ].each do |tag_name|
+      [ :amphtml, :canonical, :prev, :next, :author, :publisher, :image_src ].each do |tag_name|
         href = meta_tags.extract(tag_name)
         if href.present?
           @normalized_meta_tags[tag_name] = href
