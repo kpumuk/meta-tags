7a9bbc8f4a223245987f42ff818517c6cc42f743
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2022-12-03 14:32:44 -0500

    ActiveSupport.on_load executes the block in the context of the loaded module

--- lib/meta_tags/railtie.rb
+++ lib/meta_tags/railtie.rb
@@ -4,13 +4,13 @@ module MetaTags
   class Railtie < Rails::Railtie
     initializer "meta_tags.setup_action_controller" do
       ActiveSupport.on_load :action_controller do
-        ActionController::Base.include MetaTags::ControllerHelper
+        include MetaTags::ControllerHelper
       end
     end
 
     initializer "meta_tags.setup_action_view" do
       ActiveSupport.on_load :action_view do
-        ActionView::Base.include MetaTags::ViewHelper
+        include MetaTags::ViewHelper
       end
     end
   end
