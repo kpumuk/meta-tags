85f6f0b5ac2469d9e1d46960ad38c4060300709f
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2023-10-10 09:12:38 -0400

    Fixed buils on CI, failing with 'uninitialized constant SimpleCov::Formatter::JSONFormatter'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 
 if ENV["ENABLE_CODE_COVERAGE"]
   require "simplecov"
+  require "simplecov_json_formatter"
   SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
   SimpleCov.start do
     enable_coverage :branch
